5837ec4b7cd5647b4da6d4de2a1c533dd32d6c45
durgeshmishra18886 <54734638+durgeshmishra18886@users.noreply.github.com>
2026-08-22 17:06:00 +0530

    Time: 42 ms (80.3%), Space: 19.1 MB (64.49%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,19 +1,8 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        n = n & 0xFFFFFFFF  
-    
-        reversed_num = 0
-        
-        
+        result = 0
         for _ in range(32):
-            
-            reversed_num = (reversed_num << 1) | (n & 1)
-           
-            n >>= 1  
-            
-        
-        if reversed_num >= 0x80000000:
-            reversed_num -= 0x100000000
-            
-        return reversed_num
+            result = (result << 1) | (n & 1)
+            n >>= 1
+        return result
         
