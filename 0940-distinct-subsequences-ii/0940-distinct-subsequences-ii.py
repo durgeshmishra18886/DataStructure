03e2acd5021b828202478526ce74d4eef17f30da
durgeshmishra18886 <54734638+durgeshmishra18886@users.noreply.github.com>
2026-09-07 23:40:26 +0530

    Time: 12 ms (40.63%), Space: 19.3 MB (72.4%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -1,12 +1,12 @@
 class Solution:
     def distinctSubseqII(self, s: str) -> int:
         MOD = 10**9 + 7
-       
+
         end = [0] * 26
 
         for char in s:
             idx = ord(char) - ord("a")
-           
+
             end[idx] = (1 + sum(end)) % MOD
 
         return sum(end) % MOD
