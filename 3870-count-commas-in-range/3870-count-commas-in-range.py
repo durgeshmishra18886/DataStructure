e6605ffeee0c1b8559103e8e461c2288728bb8f1
durgeshmishra18886 <54734638+durgeshmishra18886@users.noreply.github.com>
2026-09-08 20:58:53 +0530

    Time: 0 ms (100%), Space: 19.2 MB (86.58%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -2,10 +2,9 @@ class Solution:
     def countCommas(self, n: int) -> int:
         total_commas = 0
         threshold = 1000
-    
+
         while n >= threshold:
-            total_commas += (n - threshold + 1)
+            total_commas += n - threshold + 1
             threshold *= 1000
-        
-        return total_commas 
-        
+
+        return total_commas
